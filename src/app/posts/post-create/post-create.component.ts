dc67c0c9bf500ed321102d8b4ec7b5d9c08c0aa9
apecr <alberto.eyo@gmail.com>
2020-04-16 17:24:19 +0200

    refactor() template driven to reactive
    Change the template drive approach in the create post form to a reactive approach (in the code we define the form properties)

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
@@ -7,53 +7,69 @@ import { Post } from '../post.model';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent implements OnInit{
-  enteredContent = ''
-  enteredTitle = ''
-  post: Post = null
-  isLoading = false
+export class PostCreateComponent implements OnInit {
+  enteredContent = '';
+  enteredTitle = '';
+  post: Post = null;
+  isLoading = false;
+  form: FormGroup;
 
-  private mode = 'create'
-  private postId: string = null
+  private mode = 'create';
+  private postId: string = null;
 
-  constructor(public postsService: PostsService, public route: ActivatedRoute){}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if (paramMap.has('postId')){
-        this.mode = 'edit'
-        this.postId = paramMap.get('postId')
-        this.isLoading = true
-        this.postsService.getPost(this.postId).subscribe(postData => {
-          this.isLoading = false
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+        this.postsService.getPost(this.postId).subscribe((postData) => {
+          this.isLoading = false;
           this.post = {
             id: postData._id,
-            ...postData
-          }
-        })
+            ...postData,
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content,
+          });
+        });
       } else {
-        this.mode = 'create'
-        this.postId = null
-        this.post = null
+        this.mode = 'create';
+        this.postId = null;
+        this.post = null;
       }
-    })
+    });
   }
 
-  onSavePost(form: NgForm){
-    if (form.invalid){
-      return
+  onSavePost() {
+    if (this.form.invalid) {
+      return;
     }
-    this.isLoading = true
-    if (this.mode == 'create'){
-    this.postsService.addPost(form.value.title, form.value.content)
+    this.isLoading = true;
+    if (this.mode == 'create') {
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
       this.postsService.updatePost({
         id: this.postId,
-        title: form.value.title,
-        content: form.value.content
-      })
+        title: this.form.value.title,
+        content: this.form.value.content,
+      });
     }
-    form.resetForm()
+    this.form.reset();
   }
 }
