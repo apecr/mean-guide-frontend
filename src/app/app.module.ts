c571baae6effcc9282b2a2ef89f3c0197ffa57e0
apecr <alberto.eyo@gmail.com>
2020-04-23 12:17:34 +0200

    feat() Intercept and include token authorization header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -17,6 +17,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,11 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,17 +6,29 @@ import { AuthData } from './auth-data.model';
   providedIn: 'root',
 })
 export class AuthService {
+
+  private token: string = undefined
   constructor(private httpClient: HttpClient) {}
 
+  getToken() {
+    return this.token
+  }
+
   createUser(authData: AuthData) {
-    this.httpClient.post('http://localhost:3000/api/user/signup', authData)
-    .subscribe(response => console.log(response))
+    this.httpClient.post<{token: string}>('http://localhost:3000/api/user/signup', authData)
+    .subscribe(response => {
+      console.log(response)
+      this.token = response.token
+    })
   }
 
   login(authData: AuthData){
     console.log('login')
     console.log(authData)
-    this.httpClient.post('http://localhost:3000/api/user/login', authData)
-    .subscribe(response => console.log(response))
+    this.httpClient.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    .subscribe(response => {
+      console.log(response)
+      this.token = response.token
+    })
   }
 }
