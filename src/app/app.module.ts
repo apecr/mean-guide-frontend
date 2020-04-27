8e50b14f643ad14e26c0af5cf55f479ebbe681d1
apecr <alberto.eyo@gmail.com>
2020-04-27 12:04:47 +0200

    refactor() Auth Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,28 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
+import { AuthModule } from './auth/auth.module';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
 import { HeaderComponent } from './header/header.component';
 import { PostsModule } from './posts/posts.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-    ErrorComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, ErrorComponent],
   imports: [
     AppRoutingModule,
+    AuthModule,
     BrowserModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
     HttpClientModule,
-    FormsModule,
     PostsModule,
   ],
   providers: [
